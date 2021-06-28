15a325ae066cedc3c899433ee6d51fbb6ad421d1
DaviMottaa <84351196+DaviMottaa@users.noreply.github.com>
2021-06-28 17:18:41 -0300

    Add files via upload

---
+++ Davi_24621.py
@@ -0,0 +1,64 @@
+#Question2
+Lista = ["Brasil", "EUA", "Holanda", "França"]
+
+#Question3
+print(Lista)
+
+#Question4
+for item in Lista:
+    print(item)
+
+#question5
+for item in Lista:
+    print(item + item)
+
+#Question6
+for item in Lista:
+    print(item.upper())
+
+#Question7
+print("7 ***********************************")
+print("This is my list")
+for item in Lista:
+    print(item)
+print("7 ***********************************")
+
+#Question8
+print("8 ***********************************")
+print("This is my list")
+for item in Lista:
+    print(item)
+print("This was my list")
+print("8 ***********************************")
+#Question9
+print("9 ***********************************")
+for item in Lista:
+    print("This is one item of my list")
+    print(item)
+print("9 ***********************************")
+#Question10
+print("10 ***********************************")
+for item in Lista:
+    print(item)
+    print("This was one item of my list")
+print("10 ***********************************")
+#Question11
+numbers = list(range(1,21))
+
+#Question12
+print(numbers)
+
+#Question13
+for item in range(10):
+    print("davi")
+
+#Question14
+for item in range(2,11,2):
+    print(item)
+#Question15
+print("15 ***********************************")
+for item in range(10,-2,-2):
+    print(item)
+    
+
+    
